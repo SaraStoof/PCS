ffd2598b6ca8739422cf3a57ac5f63600c7db315
Ishana <ishana.bohorey@uva.student.nl>
2025-01-23 13:02:13 +0100

    iets

--- dla_3dsim.py
+++ dla_3dsim.py
@@ -320,11 +320,6 @@ def check_grid(grid):
 grid_layer_counts = check_grid(final_grid)
 print(np.sum(grid_layer_counts))
 
-# Save to .txt file for analysis
-with open("layer_counts.txt", "w") as f:
-    for count in grid_layer_counts:
-        f.write(str(count) + "\n")
-
 # visualize grid_layer_counts in a plot
 fig, ax = plt.subplots(figsize=(6, 6))
 ax.plot(grid_layer_counts)
