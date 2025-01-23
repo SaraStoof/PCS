2b65f2228de4722a96b716b7e75916a1c3eea104
Sara Stoof <sara.stoof@student.uva.nl>
2025-01-23 13:00:23 +0100

    visualize speedup

--- test_speedup.py
+++ test_speedup.py
@@ -6,10 +6,8 @@ num_sims = ['1', '2', '5', '10', '100']
 batch_sizes = ['100', '1000']
 no_hits_maxs = ['1', '2', '5', '10', '50']
 
-with open("result_normal.txt", "w") as f:
-    f.writelines("num_sims, batch_size, no_hits_max, mold_cov_3d, mold_cov_surface\n")
-
-
+with open("speedup_results/result_normal.txt", "w") as f:
+    f.writelines("num_sims, time, batch_size, timesteps, no_hits_max, mold_cov_3d, mold_cov_surface\n")
     for n in num_sims:
         for b in batch_sizes:
             for h in no_hits_maxs:
